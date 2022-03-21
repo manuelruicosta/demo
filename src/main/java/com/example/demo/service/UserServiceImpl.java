5089eab897a643fa59cebcfca23ac9d88c14f9af
manuelruicosta <k.song10@163.com>
2022-03-21 08:11:06 +0800

    Add basic MyBatis Integration

--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dao.UserDao;
+import com.example.demo.dao.UserMapperDao;
 import com.example.demo.dto.User;
 
 @Service
@@ -14,6 +15,9 @@ public class UserServiceImpl implements UserService{
 	@Autowired
 	private UserDao userDao;
 	
+	@Autowired
+	private UserMapperDao umd;
+	
 	@Override
 	public void createUser(User user) {
 		userDao.saveUser(user);
@@ -21,7 +25,15 @@ public class UserServiceImpl implements UserService{
 
 	@Override
 	public List<User> getUserByName(String userName) {
-		return userDao.getUserByName(userName);
+//		return userDao.getUserByName(userName);
+		return umd.getUserFromName(userName);
+	}
+
+	@Override
+	public List<User> getAllUsers() {
+		// TODO Auto-generated method stub
+//		return umd.selectAll();
+		return null;
 	}
 	
 }
